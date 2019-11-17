7248d29137f1edd174cdf2f7a7fbbe64e0c11950
Paul <paul58914080@gmail.com>
2019-11-17 21:05:35 +0530

    first commit

---
+++ src/test/kotlin/com/user/UserTest.kt
@@ -0,0 +1,11 @@
+package com.user
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class UserTest {
+    @Test
+    fun `my first test`() {
+        Assertions.assertThat(true).isTrue()
+    }
+}
